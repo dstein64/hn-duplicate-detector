f39f020a8e1f6f7e6eae96bba6287966547fbddf
Dan <dstein64@users.noreply.github.com>
2020-03-24 22:45:17 -0400

    Search for and compare URLs without their protocols.
    A motivation for this is that sites that now use the "https" protocol,
    may have been submitted earlier using the "http" protocol.

--- src/content.js
+++ src/content.js
@@ -19,13 +19,28 @@ var getStoryUrl = function() {
     return storylink.href;
 };
 
+// Removes protocol portion of a URL
+var removeProtocol = function(url) {
+    var parsed = new URL(url, 'https://news.ycombinator.com/news');
+    // The protocol field includes colon but no slashes,
+    // so add 2 to also exclude slashes.
+    return parsed.href.substring(parsed.protocol.length + 2);
+};
+
 // Returns a promise that returns a list of stories if resolved and an
 // error if rejected.
 // A story includes id, date (integer timestamp), title, points, and
 // num_comments.
+// Searching and comparing URLs omits the protocol (e.g., "http").
+// A motivation for this is that sites that now use the "https"
+// protocol, may have been submitted earlier using the "http" protocol.
+// (e.g., https://news.ycombinator.com/item?id=13918888 (submitted in 2017)
+//          points to http://jlongster.com/How-I-Became-Better-Programmer
+//        https://news.ycombinator.com/item?id=22678350 (submitted in 2020)
+//          points to https://jlongster.com/How-I-Became-Better-Programmer
 var getStories = function(url) {
     var api_endpoint = 'https://hn.algolia.com/api/v1/search?query=';
-    api_endpoint += encodeURIComponent(url);
+    api_endpoint += encodeURIComponent(removeProtocol(url));
     api_endpoint += '&restrictSearchableAttributes=url';
     var promise = new Promise(function(resolve, reject) {
         var request = new XMLHttpRequest();
@@ -36,7 +51,8 @@ var getStories = function(url) {
                 var response = JSON.parse(request.response);
                 for (var i = 0; i < response.hits.length; ++i) {
                     var hit = response.hits[i];
-                    if (hit.url !== url) continue;
+                    if (removeProtocol(hit.url) !== removeProtocol(url))
+                        continue;
                     var story = {
                             'id': hit.objectID,
                             'date': hit.created_at_i,
