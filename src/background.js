5198a2eda4d290243eeb0c277abac2ceff853577
Dan <dstein64@users.noreply.github.com>
2022-02-19 14:05:03 -0500

    Don't use const for defaultOptions().
    It's accessed from options.js.

--- src/background.js
+++ src/background.js
@@ -1,8 +1,13 @@
-const defaultOptions = function() {
+// WARN: For functions that are called from the options page, proper scope is
+// necessary (e.g., using a function declaration beginning with a 'function',
+// or using a function expression beginning with 'var', but not a function
+// expression beginning with 'let' or 'const').
+
+function defaultOptions() {
     const options = Object.create(null);
     options['omitZeroComments'] = true;
     return options;
-};
+}
 
 // set missing options using defaults
 (function() {
